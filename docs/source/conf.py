3b3fd91e6f87ab9b527e258952dbcbfdb994385a
Oscar Carballal Prego <info@oscarcp.com>
2015-04-28 15:56:14 +0100

    More changes for RTD support

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
+    'sphinxcontrib.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.

efc56795b3bb1db7b626ae0e8fcf2f26f5797d21
Oscar Carballal Prego <info@oscarcp.com>
2015-04-28 15:47:02 +0100

    Updated the config of the docs for RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,8 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 # -- General configuration ------------------------------------------------
 
@@ -110,9 +112,9 @@ if not on_rtd:
         html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
         html_theme = "sphinx_rtd_theme"
     except ImportError:
-        pass
+        html_theme = 'classic'
 # html_theme = 'sphinx_rtd_theme'
-# html_theme = 'classic'
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
